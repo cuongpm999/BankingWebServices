065bb143b20e0796a40dbf53f7a2b301f530db64
Cuong Pham <phamcuongth2000@gmail.com>
2021-11-21 12:34:05 +0700

    xong tao tai khoan

---
+++ src/main/java/vn/ptit/controllers/DepositAccountController.java
@@ -0,0 +1,66 @@
+package vn.ptit.controllers;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.ptit.entities.CreatedBankAccount;
+import vn.ptit.entities.CreditAccount;
+import vn.ptit.entities.DepositAccount;
+import vn.ptit.repositories.CreatedBankAccountRepository;
+import vn.ptit.repositories.DepositAccountRepository;
+import vn.ptit.services.DepositAccountService;
+import vn.ptit.utils.HelperCreateBankAccount;
+
+@RestController
+@RequestMapping("/rest/api/deposit-account")
+public class DepositAccountController {
+	@Autowired DepositAccountRepository depositAccountRepository;
+	@Autowired DepositAccountService depositAccountService;
+	@Autowired CreatedBankAccountRepository createdBankAccountRepository;
+	
+	@GetMapping("/find-by-customer/{id}")
+	public List<DepositAccount> findByCustomer(@PathVariable("id") int id){
+		return depositAccountService.findByCustomerId(id);
+	}
+	
+	@PostMapping("/insert")
+	@Transactional
+	public DepositAccount insert(@RequestBody HelperCreateBankAccount helperCreateBankAccount){
+		DepositAccount depositAccount = helperCreateBankAccount.getDepositAccount();
+		CreatedBankAccount createdBankAccount = helperCreateBankAccount.getCreatedBankAccount();
+		
+		depositAccount = depositAccountRepository.save(depositAccount);
+		createdBankAccount = createdBankAccountRepository.save(createdBankAccount);
+		
+		return depositAccount;
+	}
+	
+	@DeleteMapping(value = "/delete-by-id/{id}")
+	public void deleteById(@PathVariable("id") String id) {
+		DepositAccount depositAccount = depositAccountRepository.findById(id).get();
+		depositAccount.setStatus(false);
+		depositAccountRepository.save(depositAccount);
+	}
+	
+	@GetMapping(value = "/find-by-id/{id}")
+	public DepositAccount findById(@PathVariable("id") String id) {
+		return depositAccountService.findByIdAndStatusTrue(id);
+	}
+	
+	@PostMapping("/update")
+	public DepositAccount update(@RequestBody DepositAccount depositAccount){
+		depositAccount = depositAccountRepository.save(depositAccount);
+		return depositAccount;
+	}
+
+}
---
+++ src/main/java/vn/ptit/services/DepositAccountService.java
@@ -0,0 +1,31 @@
+package vn.ptit.services;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Service;
+
+import vn.ptit.entities.CreditAccount;
+import vn.ptit.entities.DepositAccount;
+
+@Service
+public class DepositAccountService {
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public List<DepositAccount> findByCustomerId(int id) {
+		String jpql = "select a from DepositAccount a, CreatedBankAccount b where a.id = b.bankAccount.id and a.status = true and b.customer.id = "+id;
+		Query query = entityManager.createQuery(jpql, DepositAccount.class);
+		return query.getResultList();
+	}
+	
+	public DepositAccount findByIdAndStatusTrue(String id) {
+		String jpql = "select a from DepositAccount a where a.status = true and a.id = "+id;
+		Query query = entityManager.createQuery(jpql, DepositAccount.class);
+		return (DepositAccount) query.getResultList().get(0);
+	}
+
+}
